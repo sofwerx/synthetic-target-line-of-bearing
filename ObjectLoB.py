4c5367fac49de54a10528d5651870ed958af7d24
Ian Blenke <ian@blenke.com>
2017-10-27 07:45:30 -0400

    Deal with conditional download and extraction of model if it hasn't already been

--- ObjectLoB.py
+++ ObjectLoB.py
@@ -125,14 +125,15 @@ class PersonLoB:
     
         # In[ ]:
     
-    
-        opener = urllib.request.URLopener()
-        opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-        tar_file = tarfile.open(MODEL_FILE)
-        for file in tar_file.getmembers():
+        if not os.path.isdir(MODEL_NAME):
+          if not os.path.isfile(MODEL_FILE) :
+            opener = urllib.request.URLopener()
+            opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+          tar_file = tarfile.open(MODEL_FILE)
+          for file in tar_file.getmembers():
             file_name = os.path.basename(file.name)
             if 'frozen_inference_graph.pb' in file_name:
-                tar_file.extract(file, os.getcwd())
+              tar_file.extract(file, os.getcwd())
     
         # ## Load a (frozen) Tensorflow model into memory.
     
